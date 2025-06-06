be191bd0bc1f111863c65dd0bc3d88b0071f79be
YawoSadji <yawo.sadji@gmx.com>
2025-06-06 14:29:09 -0400

    bootstrap styles loaded

--- restaurantchatbot/src/components/MenuCarrousel.jsx
+++ restaurantchatbot/src/components/MenuCarrousel.jsx
@@ -1,16 +1,18 @@
 import Carousel from "react-bootstrap/Carousel";
 import Image from "react-bootstrap/Image";
+import jollofImgA from "../assets/jollofimgA.jpg";
+import Fufu from "../assets/Fufu.jpg";
+
 export default function MenuCarrousel() {
   return (
     <>
       <Carousel>
         <Carousel.Item>
           <Image
-            src="restaurantchatbot/src/assets/jollofimgA.jpg"
-            width={250}
-            height={250}
+            src={jollofImgA}
+            width={1000}
+            height={1000}
             alt="Jollof Chicken"
-            text="Jollof Chicken"
           />
           <Carousel.Caption>
             <h3>First slide label</h3>
@@ -19,15 +21,17 @@ export default function MenuCarrousel() {
         </Carousel.Item>
         <Carousel.Item>
           <Image
-            src="restaurantchatbot/src/assets/jollof1.webp"
-            text="Jollof Chicken"
+            src={Fufu}
+            width={1000}
+            height={1000}
+            alt="Fufu and Egusi Soup"
           />
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        {/* <Carousel.Item>
           <Image
             src="restaurantchatbot/src/assets/jollof1.webp"
             text="Jollof Chicken"
@@ -38,7 +42,7 @@ export default function MenuCarrousel() {
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
           </Carousel.Caption>
-        </Carousel.Item>
+        </Carousel.Item> */}
       </Carousel>
     </>
   );
