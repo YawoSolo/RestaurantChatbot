b4b66b3154565f6220126e66f4abdc115b1c70fd
YawoSadji <yawo.sadji@gmx.com>
2025-06-09 12:15:52 -0400

    built menu carrousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import MenuCarrousel from "./components/MenuCarrousel";
 function App() {
   return (
     <>
       <h1>Welcome to My App</h1>
+      <MenuCarrousel />
     </>
   );
 }
---
+++ src/components/MenuCarrousel.jsx
@@ -0,0 +1,40 @@
+import Carousel from "react-bootstrap/Carousel";
+import Image from "react-bootstrap/Image";
+import friedrice from "../assets/friedrice.webp";
+import jollofchicken from "../assets/jollofchicken.jpg";
+import jollofplantain from "../assets/jollofplantain.webp";
+
+export default function MenuCarrousel() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <Image src={friedrice} width={500} height={500} alt="Fried rice" />
+        <Carousel.Caption>
+          <h3>Fried Rice</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src={jollofchicken}
+          width={500}
+          height={500}
+          alt="Jollof chicken"
+        />
+        <Carousel.Caption>
+          <h3>Jollof Chicken</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src={jollofplantain}
+          width={500}
+          height={500}
+          alt="Jollof plantain"
+        />
+        <Carousel.Caption>
+          <h3>Jollof Plantain</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
