e0362192ce00d2870be82b54dcf04b26b6db9881
YawoSadji <yawo.sadji@gmx.com>
2025-06-10 21:02:44 -0400

    fixed App import in test

--- App.test.jsx
+++ App.test.jsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./src/App";
 
-test('renders welcome message', ()=>{
-    render(<App />);
-    const welcomeElement = screen.getByText("Welcome to My App");
-    expect(welcomeElement).toBeInTheDocument();
+test("renders welcome message", () => {
+  render(<App />);
+  const welcomeElement = screen.getByText("Welcome to My App");
+  expect(welcomeElement).toBeInTheDocument();
 });
